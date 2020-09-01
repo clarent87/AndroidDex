27b7cd4a5c36a0be0bea8c5e43ab230069c84111
clarent87 <45042053+clarent87@users.noreply.github.com>
2020-09-01 14:51:12 +0900

    Create anti_debug.h

---
+++ Android/anti_debug.h
@@ -0,0 +1,9 @@
+#ifndef ANTI_PTRACE_ANTI_DEBUG_H
+#define ANTI_PTRACE_ANTI_DEBUG_H
+
+void anti_debugging(); // 타이밍상 dump가 먹힐수도..
+void anti_debugging2();
+void anti_ptrace(); // 이건 단순 ptrace_me
+void thread_test();
+
+#endif //ANTI_PTRACE_ANTI_DEBUG_H
