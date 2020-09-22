aa729029fdbfc7802786cb206165ae3a92ea0575
clarent87 <45042053+clarent87@users.noreply.github.com>
2020-09-22 19:17:46 +0900

    Update jni_mem_monitor.cpp

--- Android/jni_mem_monitor.cpp
+++ Android/jni_mem_monitor.cpp
@@ -10,19 +10,23 @@
 // 5) x86에뮬에 adb 가능한지 check
 // 6) dump 준비 ( syscall 하는걸로 진행 )
 
-#include <errno.h>
-#include <poll.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/inotify.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cerrno>
 #include <unistd.h>
-#include <string.h>
-#include "monitor.h"
+#include <cstring>
+#include <pthread.h>
+#include <poll.h>
+#include <sys/inotify.h>
+#include "logger.h"
+#include "mem-monitor.h"
 
 // 종료 조건은 두지 않는다.
 // wd는 단순 error 처리만
 // task 내용 전부 읽어서 => maps / mem 따로 할당해야 할듯.. 일단은
 // path는 self까진 하드코딩이니까 받을 필요 없음.
+
+char* g_path[] = {"/proc/self/maps","/proc/self/mem"};
 
 /**
  * TODO wd도 받아서 읽힌 이벤트 확인하는 로직도 고려 but 일단은 kill을 진행.
@@ -36,21 +40,45 @@ static void handle_event(int fd ){
     ssize_t len;
     char *ptr;
 
+    LOGD("[*] in handle_event");
 
-    kill(getpid(),SIGKILL); // process id 받아서 강제 kill ( 아 fork가 resource 문제 있댔지.. kill 말고. )
+    // loop 진행 ( 뭐 글럴 필요 없어 보이긴 하지만.. )
+    while(1){
+        // 1) read event
+        len = read(fd, buf, sizeof buf);
+        if (len == -1 && errno != EAGAIN) {
+            perror("read");
+            exit(EXIT_FAILURE);
+        }
+
+        // 2) non-block read일때 => while의 종료 조건으로 활용
+        if (len <= 0) break;
+
+        // 3) for
+        for(ptr = buf; ptr < buf +len; ptr += sizeof(struct inotify_event) + event->len){ // inotify struct가 name field때문에 가변.
+            event = (const struct inotify_event *) ptr;
+
+            if (event->mask & IN_OPEN)
+                LOGD("IN_OPEN: ");
+            if (event->mask & IN_CLOSE_NOWRITE)
+                LOGD("IN_CLOSE_NOWRITE: ");
+            if (event->mask & IN_CLOSE_WRITE)
+                LOGD("IN_CLOSE_WRITE: ");
+            kill(getpid(),SIGKILL); // process id 받아서 강제 kill ( 아 fork가 resource 문제 있댔지.. kill 말고. )
+        }
+    }
     return;
 }
 
 /**
  * - 무한 루프를 도는 api
- * - thread에서 사용해야 하며
+ * - thread consumer 형태로 치환.
  * - todo : signal handler 등록해 줘야함 ( 근데 해당 시그널을 다른데서 처리하면 어쪄지? thread zombie 처리는 어떻게 하지? 일단 test용이니까 걍진행?)
  */
-void mem_monitor(){
+void* monitor(void*){
     int fd, i, poll_num;
     nfds_t nfds = 1;      // fd 하나만 볼것.
     struct pollfd fds[1]; // fd 하나만 볼것.
-
 
     // 1) inotify fd 생성
     fd = inotify_init1(IN_NONBLOCK);
@@ -59,9 +87,9 @@ void mem_monitor(){
         perror("[*] inotify_init1");
         exit(EXIT_FAILURE);
     }
+    LOGD("[*] make inotify fd\n");
 
     // 2) make path
-        // TODO 일단 단순 self만 진행
 
     // 3) watch
     for (i = 0; i < PATH_LEN; i++)
@@ -72,11 +100,12 @@ void mem_monitor(){
             exit(EXIT_FAILURE);
         }
     }
+    LOGD("[*] add watch");
 
     // 4) prepare poll
     fds[0].fd = fd;
     fds[0].events = POLLIN;
-    printf("[*] Listening for events.\n");
+    LOGD("[*] Listening for events.\n");
 
     // 5) loop poll
     while (1)
@@ -91,17 +120,31 @@ void mem_monitor(){
 
         if (poll_num > 0)
         {
-            if (fds[1].revents & POLLIN) handle_event(fd);
-
+            LOGD("[*] get poll event\n");
+            if (fds[0].revents & POLLIN) handle_event(fd);
         }
     }
-
     printf("Listening for events stopped.\n");
-    return;
+    return 0;
 }
 
-// TODO 시그널 핸들러에서 아래 내용 진행해야 함
+void mem_monitor(){
+    int status;
+    pthread_t test;
+    pthread_attr_t attrib;
+
+    pthread_attr_init(&attrib);
+    pthread_attr_setdetachstate(&attrib, PTHREAD_CREATE_DETACHED); // join 필요 없게..
+
+    pthread_create(&test,&attrib,monitor,(void*)NULL);
+    pthread_setname_np(test, "mem-monitor");
+    LOGD("[*] mem-monitor is created");
+
+}
+// TODO 시그널 핸들러에서 아래 내용 진행해야 함. ( 시그널 핸들러에서 정리? )
+// TODO 근데 debugger에 의한 정지는 어떻하기?
 ///* Close inotify file descriptor */
 // close(fd);
 // path malloc 내용 free
 //exit(EXIT_SUCCESS);
+
